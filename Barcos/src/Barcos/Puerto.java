9451b9ad45c91012e71b32c21fa6a0712044c67e
ChinCobre <1DAM@A11-PC23>
2026-01-19 12:57:23 +0100

    papa

--- Barcos/src/Barcos/Puerto.java
+++ Barcos/src/Barcos/Puerto.java
@@ -1,59 +1,31 @@
 package Barcos;
 
 import java.util.ArrayList;
-
+import Barcos.Barco;
 public class Puerto {
 	protected ArrayList<Barco> lista;
-	protected boolean entrarBarco;
-	protected String salirBarco;
-	protected double calcularPrecioViaje;
-	
-	public Puerto(ArrayList<Barco> lista, boolean entrarBarco, String salirBarco, double calcularPrecioViaje) {
+
+	public Puerto(ArrayList<Barco> lista) {
 		super();
-		this.lista = lista;
-		this.entrarBarco = entrarBarco;
-		this.salirBarco = salirBarco;
-		this.calcularPrecioViaje = calcularPrecioViaje;
-	}
-
-	public ArrayList<Barco> getLista() {
-		return lista;
-	}
-
-	public void setLista(ArrayList<Barco> lista) {
-		this.lista = lista;
-	}
-
-	public boolean isEntrarBarco() {
-		return entrarBarco;
-	}
-
-	public void setEntrarBarco(boolean entrarBarco) {
-		this.entrarBarco = entrarBarco;
-	}
-
-	public String getSalirBarco() {
-		return salirBarco;
-	}
-
-	public void setSalirBarco(String salirBarco) {
-		this.salirBarco = salirBarco;
-	}
-
-	public double getCalcularPrecioViaje() {
-		return calcularPrecioViaje;
-	}
-
-	public void setCalcularPrecioViaje(double calcularPrecioViaje) {
-		this.calcularPrecioViaje = calcularPrecioViaje;
-	}
-
-	@Override
-	public String toString() {
-		return "Puerto [lista=" + lista + ", entrarBarco=" + entrarBarco + ", salirBarco=" + salirBarco
-				+ ", calcularPrecioViaje=" + calcularPrecioViaje + "]";
+		this.lista = new ArrayList<Barco>();
 	}
 	
+	public boolean entrarBarco(Barco b) {
+		lista.add(b);
+		return true;
+	}
+	
+	public boolean salirBarco(String nombre) {
+		lista.remove(nombre);
+		return true;
+	}
+	
+	public double calcularPrecioViaje (Barco b, int horas, double precioCombustible) {
+		if () {
+			
+		}
+		
+	}
 	
 	
 }
