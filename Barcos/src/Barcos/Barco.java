936e16bd982d9d90798e01ce513caa59fcd07b8a
Adrian Ruiz <Adrin@MSI-ADRIAN>
2026-01-19 13:12:01 +0100

    actualizacion

--- Barcos/src/Barcos/Barco.java
+++ Barcos/src/Barcos/Barco.java
@@ -1,5 +1,35 @@
 package Barcos;
 
+
 public class Barco {
 
+	protected String Nombre;
+	protected int capacidadCombustible;
+	public Barco(String nombre, int capacidadCombustible) {
+		super();
+		Nombre = nombre;
+		this.capacidadCombustible = capacidadCombustible;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public int getCapacidadCombustible() {
+		return capacidadCombustible;
+	}
+	public void setCapacidadCombustible(int capacidadCombustible) {
+		this.capacidadCombustible = capacidadCombustible;
+	}
+	@Override
+	public String toString() {
+		return "Barco [Nombre=" + Nombre + ", capacidadCombustible=" + capacidadCombustible + "]";
+	}
+	
+	
+	
+	
+	
+	
 }
